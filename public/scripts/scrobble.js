59c26c607196f4e17703b22c2de6a5949d2bee97
flipsewtf <mournstera@proton.me>
2025-11-28 23:36:30 +0100

    redoing stuff

--- public/scripts/scrobble.js
+++ public/scripts/scrobble.js
@@ -1,4 +1,7 @@
 // Scrobble (Listenbrainz)
+const fixEncoding = (s) =>
+    new TextDecoder().decode(new Uint8Array([...s].map((c) => c.charCodeAt(0))));
+
 (async () => {
     const trackLink = document.querySelector('#scrobble .track');
     if (!trackLink) return;
@@ -6,7 +9,11 @@
     try {
         const res = await fetch('https://api.listenbrainz.org/1/user/flipse/listens?count=1');
         const t = (await res.json()).payload.listens?.[0]?.track_metadata;
-        if (t) trackLink.textContent = `${t.track_name} — ${t.artist_name}`;
+        if (t) {
+            const trackName = fixEncoding(t.track_name);
+            const artistName = fixEncoding(t.artist_name);
+            trackLink.textContent = `${trackName} — ${artistName}`;
+        }
     } catch (e) {
         console.error('Failed to fetch ListenBrainz data:', e);
     }
