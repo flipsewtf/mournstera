0d980495fa3fcccf3e10caf5fab8d69cdbab3913
flipsewtf <mournstera@proton.me>
2025-12-27 15:01:43 +0100

    Navigation arrangements

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,10 +1,10 @@
 // src/components/icons/index.ts
 export { default as ArrowLeft } from './ArrowLeft.svg';
-export { default as BagLink } from './BagLink.svg';
 export { default as Bluesky } from './Bluesky.svg';
 export { default as Chevron } from './Chevron.svg';
+export { default as DateCalendar } from './DateCalendar.svg';
 export { default as DateClock } from './DateClock.svg';
-export { default as EntryTag } from './EntryTag.svg';
+export { default as DateUpdated } from './DateUpdated.svg';
 export { default as Github } from './Github.svg';
 export { default as Instagram } from './Instagram.svg';
 export { default as NowBooks } from './NowBooks.svg';
@@ -12,4 +12,5 @@ export { default as NowDice } from './NowDice.svg';
 export { default as NowTv } from './NowTv.svg';
 export { default as NowVinyl } from './NowVinyl.svg';
 export { default as Rss } from './Rss.svg';
+export { default as Shovel } from './Shovel.svg';
 export { default as Sprout } from './Sprout.svg';
